74f4d4cefeedcc9dd8028f57d3e1fedcef08d056
Venkat <venkataramana.pamarthy@gmail.com>
2024-08-26 16:46:31 -0400

    added code for stage 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 function App() {
+  // const data = {}
+  let addOrUpdate = data.length>0 ? "Update" : "Add"
+  const handleListClick = function(){
+    console.log("in handleListClick()");
+  }
+
+  const handleDelete = function(){
+    console.log("in handleDelete()");
+  }
+
+  const handleSave = function(){
+    console.log("in handleSave()");
+  }
+
+  const handleCancel = function(){
+    console.log("in handleCancel()");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="Boxed">
+      <h3>
+        Customer List
+      </h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+          </tr>
+        </thead>
+        <tbody>
+           {data.map(
+              (item, index) => {
+                return (<tr key={item.id} 
+                onClick={()=>handleListClick(item)} 
+                >
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.password}</td>
+                </tr>);
+              }
+            )}
+        </tbody>
+      </table>
     </div>
-  );
+      <div className='boxed'>
+        <h3>
+          {addOrUpdate}
+        </h3>
+            <form id="customerdetails">
+              <table>
+                <tbody>
+                  <tr>
+                  <td><label htmlFor="username">Name</label></td>
+                  <td><input type = "text" id = "username" name = "username" placeholder='customer name' required></input></td>
+                  </tr>
+                  <tr>
+                  <td><label htmlFor="email">Email</label></td>
+                  <td><input type = "text" id = "email" name = "email" placeholder='name@company.com' required></input></td>
+                  </tr>
+                  <tr>
+                  <td><label htmlFor="password">Password</label></td>
+                  <td><input type = "text" id = "password" name = "password" placeholder='password' required></input></td>
+                  </tr>
+                  {/* <tr>
+                    <td colSpan="2"></td>
+                    <td><button name="delete" onClick={handleDelete}>Delete</button></td>
+                    <td><button name="save" onClick={handleSave}>Save</button></td>
+                    <td><button name="cancel" onClick={handleCancel}>Cancel</button></td>
+                  </tr> */}
+                </tbody>
+              </table>
+              <button name="delete" onClick={handleDelete}>Delete</button>
+              <button name="save" onClick={handleSave}>Save</button>
+              <button name="cancel" onClick={handleCancel}>Cancel</button>
+            </form>
+      </div>
+    </>
+      );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,22 @@
+let data = [
+    {
+      "id": 0,
+      "name": "Jack Jackson",
+      "email": "jackj@abc.com",
+      "password": "jackj"
+    },
+    {
+      "id": 1,
+      "name": "Katie Kates",
+      "email": "katiek@abc.com",
+      "password": "katiek"
+    },
+    {
+      "id": 2,
+      "name": "Glen Glenns",
+      "email": "gleng@abc.com",
+      "password": "gleng"
+    }
+  ]
+
+export default data;
